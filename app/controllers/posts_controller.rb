0fc2e5472a22e98bbc17ab33735beee7aa4478bb
saka-ri08 <262088770+saka-ri08@users.noreply.github.com>
2026-02-26 18:34:54 +0900

    データ登録画面

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,15 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    post = Post.new(post_params) # データ新規登録のために空のインスタンスを作成
+    post.save # データを保存
+    redirect_to '/top' # トップ画面へ移動
+  end
+
+  private # データ送信の安全性担保のためのストロングパラメータ
+  def post_params # post_paramsに入るデータを定義
+    params.require(:post).permit(:location, :text)
+  end
 end
