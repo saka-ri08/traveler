82aeed2731a5ae43e4ea901e7acff36f3821e298
saka-ri08 <262088770+saka-ri08@users.noreply.github.com>
2026-02-27 18:32:53 +0900

    投稿一覧画面実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,15 @@ class PostsController < ApplicationController
   def create
     post = Post.new(post_params) # データ新規登録のために空のインスタンスを作成
     post.save # データを保存
-    redirect_to '/top' # トップ画面へ移動
+    redirect_to post_path(post.id) # 移動
+  end
+
+  def index # postをすべて取得して@postsに代入
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id]) # postから1つのidを取得
   end
 
   private # データ送信の安全性担保のためのストロングパラメータ
