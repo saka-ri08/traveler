b009ca76f699e63152c6276691402b717bef39a0
saka-ri08 <262088770+saka-ri08@users.noreply.github.com>
2026-03-17 18:46:12 +0900

    投稿機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,29 @@ class PostsController < ApplicationController
   def create
     post = Post.new(post_params)
     post.save
-    redirect_to '/top'
+    redirect_to post_path(post.id)
   end
   # データ新規登録のため空のインスタンスを作成
   # データを保存、トップ画面に移動
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+  # findメソッドで1つのidを指定してデータを1つとってきて@postに入れる
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
+  end
 
   private
   def post_params
