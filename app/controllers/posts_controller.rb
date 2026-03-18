14b67be648a440b27c9319f8850d42f2c28fc6cb
saka-ri08 <262088770+saka-ri08@users.noreply.github.com>
2026-03-18 18:43:50 +0900

    画像投稿機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,7 +38,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:location, :text)
+    params.require(:post).permit(:location, :text, :image)
   end
   # post_paramsにはいるデータを定義するストロングパラメータ
 
