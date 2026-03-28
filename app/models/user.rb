26fb743321d126bd242d415ba31c03fe0923665d
saka-ri08 <262088770+saka-ri08@users.noreply.github.com>
2026-03-28 11:14:02 +0900

    フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,25 @@ class User < ApplicationRecord
   # ユーザーが削除されたら、紐づいているpostも削除する
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
+  # followerテーブルからfollowedをソースとし、フォローしているユーザーリストを定義
+  # followedテーブルからfollowerをソースとし、フォローされているユーザーリストを定義
 
   has_one_attached :profile_image
+
+  # ユーザーをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end 
 end
