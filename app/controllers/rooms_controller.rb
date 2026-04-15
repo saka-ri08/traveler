b9e728fd37ad60ec4b31ec489c87e26d09ab5c05
saka-ri08 <262088770+saka-ri08@users.noreply.github.com>
2026-04-15 19:09:38 +0900

    DM実装中

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,7 +4,7 @@ class RoomsController < ApplicationController
   def create
     @room = Room.create
     @current_entry = @room.entries.create(user_id: current_user.id)
-    @another_entry = @room.entries.create(user_id: params[:entry][:user_id])
+    @another_entry = @room.entries.create(user_id: params[:room][:entry][:user_id])
     redirect_to room_path(@room)
   end
 
